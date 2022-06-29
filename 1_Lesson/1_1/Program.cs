47b477e6d6476b4f2a8615a3d66fc14deb428137
Valentino <ghost085@mail.ru>
2022-06-29 12:42:49 +0300

    Task 1_1

--- 1_Lesson/1_1/Program.cs
+++ 1_Lesson/1_1/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, all people!");
+﻿//string num_str = Console.ReadLine();
+//int num = int.Parse(num_str);
+
+//Console.WriteLine($"{num}^2: {num * num}");
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+if (a * a == b)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
