2f358d5332184bb7990520716a8f716397b323a1
Valentino <ghost085@mail.ru>
2022-06-29 16:20:43 +0300

    Homework_1*

---
+++ 1_Lesson_Homework/extra_homework_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа вычисления модуля числа
+
+Console.Clear();
+
+int x = int.Parse(Console.ReadLine());
+
+if (x < 0)
+{
+    Console.WriteLine(-x);
+}
+else
+{
+    Console.WriteLine(x);
+}
