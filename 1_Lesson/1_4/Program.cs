6fcd43d65df4b7cc0970f0bffef265d4d63be98a
Valentino <ghost085@mail.ru>
2022-06-29 14:56:34 +0300

    Task 1_4

---
+++ 1_Lesson/1_4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+int x = int.Parse(Console.ReadLine());
+
+// 1 решение
+
+int count = -x;
+
+while (count < x + 1)
+{
+   Console.WriteLine(count); 
+   count += 1;
+}
+
+// 2 решение
+
+//int count = 0;
+
+//while (count < 2 * x + 1)
+{
+//    Console.WriteLine(-x + count);
+ //   count += 1;
+
+}
