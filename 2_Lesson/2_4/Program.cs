78f95199ed6a43ac19389f46136a509852d08af7
Valentino <ghost085@mail.ru>
2022-06-30 18:44:56 +0300

    2_4

---
+++ 2_Lesson/2_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Принимаем на вход число и проверяем кратно ли оно одновременно 7 и 23.
+
+void OneOfNum(int num_1)
+{
+    if (num_1 % 7 == 0 && num_1 % 23 == 0)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+
+OneOfNum(16);
+OneOfNum(161);
