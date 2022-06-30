58f63f17af33daad3642f780702e634e1806b71d
Valentino <ghost085@mail.ru>
2022-06-30 19:13:01 +0300

    HomeWork_1

---
+++ 2_Lesson/HomeWork_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int Number2(int num)
+{
+    return (num / 10) % 10;
+}
+int n = new Random().Next(100, 999);
+Console.WriteLine("Трёхзначное число:");
+Console.WriteLine(n);
+
+
+Console.WriteLine("Вторая цифра числа:");
+Console.WriteLine(Number2(n));
