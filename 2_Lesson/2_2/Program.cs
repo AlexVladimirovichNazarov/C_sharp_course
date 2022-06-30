cf7da4d2c964c04f162eeb2594b69e12cab5da2e
Valentino <ghost085@mail.ru>
2022-06-30 17:52:17 +0300

    2_2

---
+++ 2_Lesson/2_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вывести случайное число из отрезка [10, 99] и показать наибольшую цифру числа.
+
+int Maxnumber(int num)
+{
+    if (num % 10 > num / 10)
+    {
+        return num % 10;
+    }
+    else
+        return num / 10;
+}
+
+
+int SecondNumm = new Random().Next(10, 99);
+
+Console.WriteLine(SecondNumm);
+Console.WriteLine(Maxnumber(SecondNumm));
