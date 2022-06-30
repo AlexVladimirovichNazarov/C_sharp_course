388a15ddcd9e825b5fa4337eb8e2809285b42993
Valentino <ghost085@mail.ru>
2022-06-30 18:22:04 +0300

    2_3

---
+++ 2_Lesson/2_3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _2_2
+{
+    class Program
+    {
+        static int CheckinValues(in int val1, in int val2)
+        {
+            return (val1 % val2);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            int value1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите первое число:");
+            int value2 = int.Parse(Console.ReadLine());
+
+            int val = CheckinValues(in value1, in value2);
+
+            if (val == 0)
+            {
+                Console.WriteLine("Числа кратные");
+            }
+            else
+            {
+                Console.WriteLine($"Остаток от деления {val}");
+            }
+        }
+
+
+
+    }
+
+
+}
+
