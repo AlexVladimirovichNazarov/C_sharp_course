f3cede22b1cefd38645e98f1ba1d2a99a9b40a53
Valentino <ghost085@mail.ru>
2022-06-30 19:35:19 +0300

    HomeWork_2

---
+++ 2_Lesson/HomeWork_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+void Number3_2(int num)
+{
+    Console.WriteLine("Первая и третья цифры числа:");
+    Console.WriteLine($"{num / 100}{num % 10}");
+}
+int n = new Random().Next(100, 999);
+Console.WriteLine("Трёхзначное число:");
+Console.WriteLine(n);
+
+Number3_2(n);
