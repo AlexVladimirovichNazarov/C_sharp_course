489407bace054b48f75172ebf5f119b46c38efc5
Valentino <ghost085@mail.ru>
2022-07-02 18:45:59 +0300

    HomeWork_4

---
+++ 2_Lesson/HomeWork_4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+void Week(int num)
+{
+    // int index = 1;
+
+    if (num == 6)
+    {
+        Console.WriteLine("Ура выходной!");
+    }
+    else if (num == 7)
+    {
+        Console.WriteLine("Ура выходной!");
+    }
+    else
+    {
+        Console.WriteLine("Будни. Хнык, хнык... Опять идни на работу.");
+    }
+   
+}
+
+int n = new Random().Next(1, 7);
+Console.WriteLine("День недели:");
+Console.WriteLine(n);
+
+Week(n);
