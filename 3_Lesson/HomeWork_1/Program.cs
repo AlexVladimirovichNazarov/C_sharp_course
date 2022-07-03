ea923c15a8b0edd60f9e28b94a20d77bb40f87e2
Valentino <ghost085@mail.ru>
2022-07-03 22:52:50 +0300

    HommmeWork_1

---
+++ 3_Lesson/HomeWork_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+void palindrome(int num)
+{
+    Console.WriteLine(num);
+    if (num / 10000 == num % 10 && num / 1000 % 10 == num / 10 % 10) 
+    Console.WriteLine("Да");
+    else
+    Console.WriteLine("Нет");
+}
+
+palindrome(14212);
+palindrome(12821);
+palindrome(23432);
