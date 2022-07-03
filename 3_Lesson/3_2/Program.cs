ee615a5e8eb4456d826301edf7f9f9510b5fdfce
Valentino <ghost085@mail.ru>
2022-07-03 22:59:23 +0300

    HommmeWork_2

--- 3_Lesson/3_2/Program.cs
+++ 3_Lesson/3_2/Program.cs
@@ -5,7 +5,7 @@
 // A (3,6); B (2,1) -> 5,09
 // A (7,-5); B (1,-1) -> 7,21
 
-double XY(double x1, double x2, double y1, double y2)
+double XY(double x1, double y1, double x2, double y2)
 {
 return Math.Sqrt(Math.Pow(x2-x1, 2)+Math.Pow(y2-y1, 2)); // Математическая библионека, квадратный корень из и квадрат...
 }
