719e586e6646cd5c18ecac092021685d56b097c3
Valentino <ghost085@mail.ru>
2022-07-05 17:36:38 +0300

    4_3

---
+++ 4_Lesson/4_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Принимаем число А и передаём произведение от 1 до А
+
+int MultNum(int num)
+{
+int all_mult = 1;
+for(int i = 1; i <= num; i++)
+{
+    all_mult *=  i;
+}
+return all_mult;
+}
+
+Console.WriteLine(MultNum(4));
+Console.WriteLine(MultNum(5));
