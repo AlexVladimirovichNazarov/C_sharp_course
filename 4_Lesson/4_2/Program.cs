734f2462d46f2137e1a5019296d3e7d8a7d62dc7
Valentino <ghost085@mail.ru>
2022-07-05 17:28:00 +0300

    4_2

---
+++ 4_Lesson/4_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Принимаем число и выдаём кол-во цифр в чиле.
+
+int CountNum(int num) 
+{
+    string result = num.ToString(); // Длина строки
+    return result.Length;
+}
+
+Console.WriteLine(CountNum(456));
