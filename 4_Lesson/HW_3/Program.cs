097ceb7298f68810794902f489454e6bada3a3ac
Valentino <ghost085@mail.ru>
2022-07-05 19:18:58 +0300

    hw_3

---
+++ 4_Lesson/HW_3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран ОТСОРТИРОВАННЫЕ.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] arr = { 1, 2, 5, 7, 19, 6, 1, 33 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+
+SelectionSort(arr);
+PrintArray(arr);
