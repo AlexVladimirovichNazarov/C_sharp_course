a2bb19ec63940ed4c3cd7751ba14efe7c497e6ea
Valentino <ghost085@mail.ru>
2022-07-14 22:26:50 +0300

    hw_3

--- 4_Lesson/HW_3/Program.cs
+++ 4_Lesson/HW_3/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран ОТСОРТИРОВАННЫЕ.
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран ОТСОРТИРОВАННЫЕ arr arr_1 заполненные случайными цифрами.
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
 int[] arr = { 1, 2, 5, 7, 19, 6, 1, 33 };
@@ -31,7 +31,24 @@ void SelectionSort(int[] array)
     }
 }
 
+ int [] EightMass()
+    {
+        int size = 8;
+
+        int[] arr = new int[size];
+
+        for (int i =  0; i < size; i++)
+        {
+            arr[i] = new Random().Next(1, 100);
+        }
+        return arr;
+    }
+
+
 PrintArray(arr);
 
 SelectionSort(arr);
 PrintArray(arr);
+
+int[] arr_1 = EightMass();
+PrintArray(arr_1);
