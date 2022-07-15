47b64bc6fc3fcef14bb3c8d2d42736650ea70363
Valentino <ghost085@mail.ru>
2022-07-15 16:16:32 +0300

    5_3

---
+++ 5_Lesson/5_3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте массив от 0 до 123. Найдите количество элементов массива которое лежит на отрезке [10, 99].
+
+void Print(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(0, 123); // верхняя граница не включается
+    }
+    return arr;
+}
+
+void SomeNum(int[] arr)
+{
+    int Number_1 = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < 99 && arr[i] > 10) Number_1 +=1;
+    }
+
+    Console.WriteLine(Number_1);
+}
+
+int[] arr_1 = MassNums(12);
+Print(arr_1);
+
+SomeNum(arr_1);
