ba4445c6b257af2c5210605771c353d8ca32fd85
Valentino <ghost085@mail.ru>
2022-07-15 17:30:06 +0300

    hw_1

---
+++ 5_Lesson/HW_1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+void Print(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(100, 999); // верхняя граница не включается
+    }
+    return arr;
+}
+
+void NumChot(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if ((arr[i] % 2) == 0) count ++;
+    }
+
+    Console.WriteLine(count);
+}
+
+int[] arr_1 = MassNums(4);
+Print(arr_1);
+
+NumChot(arr_1);
